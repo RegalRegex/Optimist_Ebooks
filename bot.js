fcbcc0d61f39d71373b3d9c1e17e2c03704d28fa
Rafiq Ramadan III <Rafiq@Rafiqs-MacBook-Pro.local>
2017-08-28 10:57:38 -0500

    First commit

---
+++ bot.js
@@ -0,0 +1,8 @@
+//bot.jst
+var twit = require('twit');
+var config = require('./config.js');
+var Twitter = new twit(config);
+
+Twitter.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+  console.log(data)
+})
