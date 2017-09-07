4bbb57c4898c79f125ed941af0d1ce2566ef1a2f
OptimisticShibe <britishdragons123@gmail.com>
2017-09-07 16:09:33 -0500

    Bot working. Needs better Regex editing

--- bot.js
+++ bot.js
@@ -6,37 +6,38 @@ const Markov = require('markov-strings');
 var T = new twit(config);
 const data = [];
 const options = {
-  maxLength: 140,
+  maxLength: 130,
   minWords: 10,
-  minScore: 50,
+  minScore: 25,
   checker: sentence => {
     return sentence.endsWith(''); // I want my tweets to end with a dot.
   }
 };
 
-// Instantiate the generator 
-//const markov = new Markov(data, options);
+// TODO: Check if random word drop is working
+// TODO: Automate
+// TODO: Get rid of underscores in tweets
+// TODO: check 
 
 getTweets(T).then(tweetIt);
-//tweetIt(data);
-//setInterval(tweetIt, 1000 * 60 * 5)
-//setInterval(getTweets, 1000 * 60 * 60 * 24)
-
 
 var tweet;
 var user_tweets = [];
 
 /* REGULAR EXPRESSIONS HERE */
 var re1 = /\b(RT|MT) .+/; // RTs
-var re2 = /(^|\s)(#[a-z\d-]+)/; // Hashtags
+var re2 = /(^|\s)(#[a-z\d-]+)/gi; // Hashtags
 var re3 = /\n/; // Extra lines
 var re4 = /\"|\(|\)/; // Attribution
-var re5 = /\s*(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi
+var re5 = /\s*(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi // Removes hyperlinks
+var re6 = /@\w{1,15}/gi // Removes @ mentions
+var re7 = /(^|\s)(_[a-z\d-]+)/gi // Removes underscores
+// /@\w{1,15}/g alternative @ regex from Shep
 
-var regexes = [re1, re2, re3, re4, re5];
+var regexes = [re1, re2, re3, re4, re5, re6, re7];
 // str.replace(regexp|substr, newSubstr|function)
 function filterTweet(rawTweet) { // filters tweets with regex
-  var tmp;
+  let tmp;
   var tweetNew;
   for (let i = 0; i < regexes.length; i++) {
     tmp = rawTweet.replace(regexes[i], '');
@@ -60,20 +61,20 @@ async function getTweets(T) { // collects tweets and edits
   let freshBatch;
   let uniqueTweets = [];
   let result;
+  let tempMaxId;
   // while sourceTweets isn't full yet
-  while (sourceTweets.length < 1000) {
+  while (sourceTweets.length < 3600) {
     result = await T.get('statuses/user_timeline', params);
     freshBatch = result.data;
     console.log("Array Length: " + freshBatch.length + " | max_id: " + params.max_id);
     // get oldest ID, and set params.max_id
-    if (freshBatch.length === 0) {
-      console.log("FreshBatch empty. Randomly readjusting");
-      let tempNum = (Math.random() * (10 - 1) + 1)
-      params.max_id = sourceTweets[sourceTweets.length - tempNum];
-      continue;
+    if (freshBatch.length === 0 || freshBatch.length - 1 === 0) {
+      console.log("Not enough tweets. Breaking");
+      break;
     }
     else {
       params.max_id = freshBatch[freshBatch.length - 1].id - 1;
+      tempMaxId = freshBatch[freshBatch.length - 5].id - 1;
       // filter out duplicate tweets in new batch
       uniqueTweets = freshBatch.map(tweet => filterTweet(tweet.text))
         .filter(tweet => tweet.length > 0);
@@ -98,7 +99,7 @@ function tweetIt(sourceTweets) {
   console.log('The length of the source:', sourceTweets.length);
   //console.log(sourceTweets);
   // Build the corpus
-  console.log(sourceTweets[0]);
+  //console.log(sourceTweets[0]);
   markov.buildCorpus()
     .then(() => {
 
@@ -106,82 +107,53 @@ function tweetIt(sourceTweets) {
       const tweets = [];
       for (let i = 0; i < 10; i++) {
         tweets.push(markov.generateSentence());
-        //console.log(tweets[i]);
+        //console.log(results[i].string);
         // .then(result => {
         //   tweets.push(result);
         // });
       }
+      Promise.all(tweets).then(tweets => {
+        for (let tweet of tweets) {
+            console.log(tweet.string);
+        }
+      });
       Promise.all(tweets)
         .then(results => {
-            T.post('statuses/update', { status: results.pop().string }, tweeted);
+          let actualTweet;
+          let min = 1;
+          let max = 5;
+          let dropWordRand;
+          actualTweet = results.pop().string;
 
-            function tweeted(err, data, response) {
-              if (err) {
-                console.log("Something went wrong!");
-                console.log(err);
-              } else {
-                console.log(response.data);
-              }
-
+          function dropWordMath(min, max) {
+            dropWordRand = Math.random() * (max - min) + min;
+            if ((dropWordRand === 4) && (actualTweet.test(/(in|to|from|for|with|by|our|of|your|around|under|beyond)\s\w+$/) == true)){
+                dropWord();
             }
-    });
+          }
 
-      // Promise.all(arrayOfPromises)
-      // .then(arrayOfValues => {
-      //   console.log(arrayOfValues)
-      // });
-      // console.log(tweets);
+          function dropWord()  {
+            actualTweet = actualTweet.replace(/\s\w+.$/, '');
+            console.log("Dropping last word randomly");
+            return actualTweet;
+          }
+          
+          dropWordMath(min, max);
+          T.post('statuses/update', { status: actualTweet }, tweeted);
+
+          function tweeted(err, data, response) {
+            if (err) {
+              console.log("Something went wrong!");
+              console.log(err);
+            } else {
+              console.log(response.data);
+            }
+
+          }
+        });
 
     });
 }
-
-
-
-
-
-// data.push(sourceTweets)
-
-/*function tweetIt(markov) {
-  // Build the corpus
-  markov.buildCorpus()
-    .then(() => {
-
-      // Generate some tweets
-      var markovTweets = [];
-      for (let i = 0; i < 10; i++) {
-        markov.generateSentence()
-          .then(result => {
-            markovTweets.push(result);
-            console.log(markovTweets);
-          });
-        }
-    //  console.log(markovTweets);
-    // return markovTweets;
-    })
-    .then((markovTweets) => {
-      var e_tweet;
-      e_tweet = markovTweets.pop();
-      console.log(e_tweet);
-      return e_tweet;
-    });*/
-
-  /*
-  e_tweet = { status: sourceTweets.pop() };
-  T.post('statuses/update', e_tweet, tweeted)
- 
-  function tweeted(err, data, response) {
-    if (err) {
-      console.log("Something went wrong!");
-    } else {
-      console.log(data);
-    }
- 
-  }
-}*/
-
-
-
-
 
 
 
