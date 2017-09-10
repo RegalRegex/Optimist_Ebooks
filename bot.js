5f2823c5a048be68a9fb2649514782fe49db9e19
OptimisticShibe <britishdragons123@gmail.com>
2017-09-10 12:09:15 -0500

    latest bot iteration

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const data = [];
 const options = {
   maxLength: 130,
   minWords: 3,
-  minScore: 15,
+  minScore: 25,
   checker: sentence => {
     return sentence.endsWith(''); // I want my tweets to end with a dot.
   }
