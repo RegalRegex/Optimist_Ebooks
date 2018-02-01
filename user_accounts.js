70fcde143d4b12fc80724da6e3ed1a178672c312
Rafiq Ramadan <rafiq.ramadan@accenturefederal.com>
2018-02-01 09:56:37 -0600

    removed Aivi

--- user_accounts.js
+++ user_accounts.js
@@ -1,3 +1,3 @@
 module.exports = {
-user: ['SharpiBees', 'RegalReindeer', 'KaylaMod', 'KaylurMod', 'sylargenesis', 'CytoSprout', 'Luck_e_Duck', 'Aivesse_Arts', 'timidstareli', 'insightstaff']
+user: ['SharpiBees', 'RegalReindeer', 'KaylaMod', 'KaylurMod', 'sylargenesis', 'CytoSprout', 'Luck_e_Duck', 'timidstareli', 'insightstaff']
 }
