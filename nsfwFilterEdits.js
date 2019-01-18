414989570d0502f98b5a10a6eddde32b5323c741
OptimisticShibe <britishdragons123@gmail.com>
2019-01-17 23:23:49 -0600

    reverting

--- nsfwFilterEdits.js
+++ nsfwFilterEdits.js
@@ -1,27 +1,43 @@
 module.exports = {
-  isNsfw: isNsfw,
-  nsfwReplace: nsfwReplace
+    isNsfw: isNsfw,
+    nsfwReplace: nsfwReplace
 };
 
-const nsfwExternal = require("./nsfwList.js");
+const nsfwExternal = require('./nsfwList.js');
 
 function nsfwReplace(tweetNew) {
-  var nsfwFilterRegexes = nsfwExternal.nsfwArray;
-  var sfwFilterReplace = nsfwExternal.sfwArray_S;
-  // Plurals currently does not work
-  var sfwFilterPluralReplace = nsfwExternal.sfwArray_P;
 
-  for (let i = 0; i < nsfwFilterRegexes.length; i++) {
-    if (nsfwFilterRegexes[i].test(tweetNew) == true) {
-      //let regPosition;
-      //regPosition = tweetNew.search(nsfwFilterRegexes[i]);
-      //if (/\w+s\b/.test(tweetNew[regPosition]) == true) {
-      //    tweetNew = tweetNew.replace(nsfwFilterRegexes[i], sfwFilterPluralReplace[i]);
-      //}
-      //else {
-      tweetNew = tweetNew.replace(nsfwFilterRegexes[i], sfwFilterReplace[i]);
-      //}
+    var nsfwFilterRegexes = nsfwExternal.nsfwArray;
+    var sfwFilterReplace = nsfwExternal.sfwArray_S;
+    // Plurals currently does not work
+    var sfwFilterPluralReplace = nsfwExternal.sfwArray_P;
+
+    for (let i = 0; i < nsfwFilterRegexes.length; i++) {
+        if (nsfwFilterRegexes[i].test(tweetNew) == true) {
+            //let regPosition;
+            //regPosition = tweetNew.search(nsfwFilterRegexes[i]);
+            //if (/\w+s\b/.test(tweetNew[regPosition]) == true) {
+            //    tweetNew = tweetNew.replace(nsfwFilterRegexes[i], sfwFilterPluralReplace[i]);
+            //}
+            //else {
+            tweetNew = tweetNew.replace(nsfwFilterRegexes[i], sfwFilterReplace[i]);
+            //}
+            console.log("UPDATED: " + tweetNew);
+        }
     }
-  }
-  return tweetNew;
+    return tweetNew;
 }
+
+function isNsfw(actualTweet){
+    let isSafe = true;
+    console.log(isSafe);
+    //this.actualTweet = actualTweet;
+    if (nsfwExternal.nsfw.test(actualTweet) == true) {
+        isSafe = false;
+        console.log(isSafe);
+        // actualTweet = nsfwReplace(actualTweet);
+        console.log(actualTweet);
+  }
+  return isSafe;
+}
+
