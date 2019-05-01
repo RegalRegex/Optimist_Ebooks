73e5ea37de83bdd37ba073914ab12fb6f6f4393a
OptimisticShibe <britishdragons123@gmail.com>
2019-04-30 23:26:11 -0500

    fixed myth name

--- user_accounts.js
+++ user_accounts.js
@@ -1,5 +1,5 @@
 module.exports = {
 user: ['SharpiBees', 'hornyhoneyhell', 'RegalReindeer', 'regalfuckdeer', 'EThePaintDragon', 'FossaFussy', 'OhNoItsWindy', 'oohtitties', 'therestokube', 'TheAshpit',
-'saltyfauxhawk', 'limesquares', 'limecircles', 'melisadraws', 'LynMyth', 'MythKeeper'],
+'saltyfauxhawk', 'limesquares', 'limecircles', 'melisadraws', 'LynMyth', 'Myth_Keeper_'],
 emoji: ['🐝', '🍯', '🦐', '💦', '🐴', '😈', '⛈', '🍑', '👌', '🔥', '👀', '🌱', '🦀', '🌑', '🐒', '😏']
 }
