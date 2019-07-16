68dc00f26b21c6603ed7d281f57f326dc7b5bcbb
Regal <britishdragons123@gmail.com>
2019-07-16 15:40:29 -0500

    added Elek

--- user_accounts.js
+++ user_accounts.js
@@ -1,4 +1,4 @@
 module.exports = {
-user: ['SharpiBees', 'RegalReindeer', 'KaylaMod', 'KaylurMod', 'sylargenesis', 'CytoSprout', 'Luck_e_Duck', 'ehartworks', 'EThePaintDragon', 'OhNoItsWindy', 'ManticoreAss', 'Azuiden'],
-emoji: ['🐊', '🦌', '😎', '🍅', '🔪', '🌱', '🦆', '💜', '🐴', '🐉', '🤠', '🦎']
+user: ['SharpiBees', 'RegalReindeer', 'KaylaMod', 'KaylurMod', 'sylargenesis', 'CytoSprout', 'Luck_e_Duck', 'ehartworks', 'EThePaintDragon', 'OhNoItsWindy', 'ManticoreAss', 'Azuiden', 'elektronxz'],
+emoji: ['🐊', '🦌', '😎', '🍅', '🔪', '🌱', '🦆', '💜', '🐴', '🐉', '🤠', '🦎', '🔫']
 }
