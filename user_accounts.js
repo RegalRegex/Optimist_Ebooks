145290c38a750fd7512ed965b4daf11cddaab194
Rafiq Ramadan <rafiq.ramadan@accenturefederal.com>
2019-07-26 14:32:49 -0500

    updated regal accounts

--- user_accounts.js
+++ user_accounts.js
@@ -1,5 +1,5 @@
 module.exports = {
-user: ['SharpiBees', 'hornyhoneyhell', 'RegalReindeer', 'regalfuckdeer', 'EThePaintDragon', 'FossaFussy', 'OhNoItsWindy', 'oohtitties', 'therestokube', 'TheAshpit',
-'saltyfauxhawk', 'limesquares', 'limecircles', 'melisadraws', 'LynMyth', 'Myth_Keeper_'],
-emoji: ['🐝', '🍯', '🦐', '💦', '🐴', '😈', '⛈', '🍑', '👌', '🔥', '👀', '🌱', '🦀', '🌑', '🐒', '😏']
+    user: ['SharpiBees', 'hornyhoneyhell', 'RegalRegex', 'CumLizard', 'EThePaintDragon', 'FossaFussy', 'OhNoItsWindy', 'oohtitties', 'therestokube', 'TheAshpit',
+        'saltyfauxhawk', 'limesquares', 'limecircles', 'melisadraws', 'LynMyth', 'Myth_Keeper_'],
+    emoji: ['🐝', '🍯', '🦐', '💦', '🐴', '😈', '⛈', '🍑', '👌', '🔥', '👀', '🌱', '🦀', '🌑', '🐒', '😏']
 }
