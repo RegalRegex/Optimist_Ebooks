e446146e52510d777d2ac5404c851cb6c599188c
OptimisticShibe <britishdragons123@gmail.com>
2019-07-29 23:12:41 -0500

    added Josh

--- user_accounts.js
+++ user_accounts.js
@@ -1,4 +1,4 @@
 module.exports = {
-    user: ['SharpiBees', 'RegalRegex', 'KaylaMod', 'KaylurMod', 'sylargenesis', 'CytoSprout', 'Luck_e_Duck', 'ehartworks', 'EThePaintDragon', 'OhNoItsWindy', 'ManticoreAss', 'Azuiden', 'elektronxz'],
-    emoji: ['🐊', '🦌', '😎', '🍅', '🔪', '🌱', '🦆', '💜', '🐴', '🐉', '🤠', '🦎', '🔫']
+    user: ['SharpiBees', 'RegalRegex', 'KaylaMod', 'KaylurMod', 'sylargenesis', 'CytoSprout', 'Luck_e_Duck', 'ehartworks', 'EThePaintDragon', 'OhNoItsWindy', 'ManticoreAss', 'Azuiden', 'elektronxz', 'FurbyFarts'],
+    emoji: ['🐊', '🦌', '😎', '🍅', '🔪', '🌱', '🦆', '💜', '🐴', '🐉', '🤠', '🦎', '🔫', '🐔']
 }
